9d5e900114aa5e07a30e7bbacb768f0c8b647143
alexlemberg90 <alexlemberg90@gmail.com>
2021-01-19 22:09:20 +0200

    Angular

---
+++ src/data/usersArr.ts
@@ -0,0 +1,27 @@
+export const    users = [
+  {
+    id: 1,
+    name: "Leanne Graham",
+    username: "Bret",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 2,
+    name: "Ervin Howell",
+    username: "Antonette",
+    email: "Shanna@melissa.tv",
+  },
+  {
+    id: 3,
+    name: "Clementine Bauch",
+    username: "Samantha",
+    email: "Nathan@yesenia.net",
+  },
+  {
+    id: 4,
+    name: "Patricia Lebsack",
+    username: "Karianne",
+    email: "Julianne.OConner@kory.org",
+  }
+];
+
